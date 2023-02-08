e8c394454dd722dd8f7ea2e0b0f0fb70d6482e2a
Lukas Prommegger <118353636+LukasPrommeg@users.noreply.github.com>
2023-02-08 08:59:05 +0100

    Add files via upload

---
+++ Webserver/src/test/java/com/StretPro/Drehteller/DrehtellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StretPro.Drehteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrehtellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
